ead58225772415b3e861901035768861a8957793
FerMarcelin <f.marcelin.leyva02@gmail.com>
2021-08-26 16:23:40 -0500

    Se modificaron los serializadores para que en ellos se encontrara la lógica

--- Users/api/serializers/user_serializers.py
+++ Users/api/serializers/user_serializers.py
@@ -12,7 +12,8 @@ class UserSerializer(Serializer):
     email = EmailField(allow_null=False)
     password = CharField(allow_null=False)
     phone = CharField(allow_null=False)
-    #state = IntegerField()  # porque sino indico lo contrario sera una persona fisica
+
+    # state = IntegerField()  #
 
     def validate(self, attrs):
         if User.objects.filter(email=attrs['email']).exists():
@@ -23,6 +24,48 @@ class UserSerializer(Serializer):
         return attrs
 
     def create(self, validated_data):
-        #validated_data.pop('state')
+        # validated_data.pop('state')
         return User.objects.create_user(**validated_data)
+
+
+class CheckPasswordSerializer(Serializer):
+    user = SerializerMethodField
+
+    def get_user(self, obj: user, pwd: str):
+        query_set = User.objects.get(email=obj.email)
+        # print(obj.password)
+        # print(pwd)
+        raw_pass = obj.check_password(pwd)
+        # print(raw_pass)
+        connection = ConnectionState.objects.get(id=2)
+        if raw_pass:
+            obj.state = connection
+            obj.save()
+            # print(obj.state.state)
+            return True
+        return False
+
+
+class CheckStateSerializer(Serializer):
+    user = SerializerMethodField
+
+    def get_user(self, obj: user):
+        query_set = User.objects.get(email=obj.email)
+        if obj.state.state == "inline":
+            return True
+        return False
+
+
+class ChangeStateSerializer(Serializer):
+    user = SerializerMethodField
+
+    def get_user(self, obj: user):
+        connection = ConnectionState.objects.get(id=1)
+        before = obj.state
+        obj.state = connection
+        #print(user.state.state)
+        obj.save()
+        if before != obj.state:
+            return True
+        return False
 
