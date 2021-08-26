6cce88e05ba92ed1676cea526541e5a92a3bc707
FerMarcelin <f.marcelin.leyva02@gmail.com>
2021-08-25 20:29:30 -0500

    Creación manual de los dos tipos de conexión en la tabla ConnectionState

--- Users/tests.py
+++ Users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from ProyectoFinalPOLI.wsgi import *
+from Users.models import User, ConnectionState
 
 # Create your tests here.
+
+#connection_type = ConnectionState.objects.create(state="offline")
+#connection_type2 = ConnectionState.objects.create(state="inline")
+
+p1 = User.objects.create_user(name='Calu', last_name='Marcelin', email='calu@correo.com', phone='+52554595876', password='calu123')
